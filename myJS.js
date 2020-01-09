744c12ce82c29a0bfec948472958492e0a94ecc6
Walter Hoerman <whoerman@gmail.com>
2020-01-09 14:24:39 -0500

    Adding arrays

--- myJS.js
+++ myJS.js
@@ -1,15 +1,26 @@
-/* getting the data from checkboxes when button clicked */
+/* setting up the character arrays */
+let lowerCaseArray = ["a","b","c","d","e"];
+let upperCaseArray = ["A","B","C","D","E"];
+let numericArray = ["1","2","3","4","5"];
+let specialArray = ["~","!","#","*"];
+let sampleArry = [];
 
-let lowerOption = document.getElementById("lowerCase")
-let upperOption = document.getElementById("upperCase")
-let numericOption = document.getElementById("numericCharacters")
-let specialOption = document.getElementById("specialCharacters")
-let copyOption = document.getElementById("copyToClipboard")
+/* getting string length from range slider */
+let passwordNumber = document.getElementById("passwordLength")
+
+
+/* getting the data from checkboxes when button clicked */
+let lowerOption = document.getElementById("lowerCase");
+let upperOption = document.getElementById("upperCase");
+let numericOption = document.getElementById("numericCharacters");
+let specialOption = document.getElementById("specialCharacters");
+let copyOption = document.getElementById("copyToClipboard");
 
 
 document.getElementById("submit-btn").addEventListener("click", function (event) {
     // pulling out the event object and preventing the browser from refreshing the page
     event.preventDefault();
+    console.log("The password length is" + passwordNumber);
     console.log("Lower case is " + lowerOption.checked);
     console.log("Upper case is " + upperOption.checked);
     console.log(" Numeric is " + numericOption.checked);
