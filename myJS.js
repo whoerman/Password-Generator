4cff67b5f988ba42aac2a70135a6bc5f2be7a69c
Walter Hoerman <whoerman@gmail.com>
2020-01-10 11:22:55 -0500

    adding the loop for password selection

--- myJS.js
+++ myJS.js
@@ -1,18 +1,20 @@
 /* setting up the character arrays */
-let lowerCaseArray = ["a","b","c","d","e"];
-let upperCaseArray = ["A","B","C","D","E"];
-let numericArray = ["1","2","3","4","5"];
-let specialArray = ["~","!","#","*"];
-let sampleArry = [];
+let lowerCaseArray = ["a", "b", "c", "d", "e"];
+let upperCaseArray = ["A", "B", "C", "D", "E"];
+let numericArray = ["1", "2", "3", "4", "5"];
+let specialArray = ["~", "!", "#", "*"];
+let sampleArray = [];
+let passwordInitiator = 0;
+let currentPWitem = ""
 
-/* getting passwrod length from range slider */
+console.log("password intitator before click is " + passwordInitiator);
 
-let passwordNumber = document.getElementById("passwordLength")
-let passwordSizeNum = document.getElementById("passwordLength")
-console.log(passwordNumber)
-console.log(passwordSizeNum)
+/* getting passwrod length from range slider 
+will do a place holder value of 20 until i figure it out*/
 
-/* getting the data from checkboxes when button clicked */
+let passwordNumber = 5
+
+/* setting the data from checkboxes when button clicked */
 let lowerOption = document.getElementById("lowerCase");
 let upperOption = document.getElementById("upperCase");
 let numericOption = document.getElementById("numericCharacters");
@@ -23,16 +25,49 @@ let copyOption = document.getElementById("copyToClipboard");
 document.getElementById("submit-btn").addEventListener("click", function (event) {
     // pulling out the event object and preventing the browser from refreshing the page
     event.preventDefault();
-    console.log("The password length is" + passwordNumber);
+    let passwordInitiator = 1
+    console.log("The password length is " + passwordNumber);
     console.log("Lower case is " + lowerOption.checked);
     console.log("Upper case is " + upperOption.checked);
     console.log(" Numeric is " + numericOption.checked);
     console.log("Special is " + specialOption.checked);
     console.log("Copy to Clipboard is " + copyOption.checked);
+    console.log("The intial Sample array is " + sampleArray);
+    console.log("password intitator after click is " + passwordInitiator);
 
-    if (lowerOption.checked == true) {
-        console.log("true for lower case")
+    /* adding the selected text type arrays together*/
+    if (passwordInitiator = 1) {
+        if (lowerOption.checked == true) {
+            sampleArray = sampleArray.concat(lowerCaseArray)
+            
+        console.log(sampleArray)
+        }
+        if (upperOption.checked == true) {
+            sampleArray = sampleArray.concat(upperCaseArray)
+            
+        console.log(sampleArray)
+        }
+        if (numericOption.checked == true) {
+            sampleArray = sampleArray.concat(numericArray)
+            
+        console.log(sampleArray)
+        }
+        if (specialOption.checked == true) {
+            sampleArray = sampleArray.concat(specialArray)
+            
+        console.log(sampleArray)
+        }
+
+        //genertaing the random selections from the concat array
+
+       for (i=0; i < passwordNumber; i++) {
+            currentPWitem = sampleArray [Math.floor(Math.random() * Math.floor(sampleArray.length))];
+
+            console.log(currentPWitem)
+        }
+
+
     } else {
-        console.log ("false for lower case")
     }
 });
+
