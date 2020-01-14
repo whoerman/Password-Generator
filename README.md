85f840a7652c6024d9a448703d5fed52173bfd6e
Walter Hoerman <whoerman@gmail.com>
2020-01-14 06:53:07 -0500

    cleaning up

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 # Password-Generator
-random Password generator based on selections
 
-password length can be from 8 to 128 characters
-password can include:
-lower case letters
-upper case letter
-numeric charcters
-special characters
-above options are selected via rangle slider and checkboxes
+This program develops a random password based on parameters inputed but the user
 
-if no options are picked, a default alert warns the user
+Password length is chosen using a range slide
+    The length can be from 8 to 128 characters
 
-currently, the password result is displayed in an alert
+Password characters are chosen using checkboxes
+    Password characters options include:
+        lower case letters
+        upper case letter
+        numeric charcters
+        special characters
+
+        if no character options are picked, an error alert appears
+
+When generate button clicked, a password is generate based on length and characters
+
+When copy button is clicked, the password is copied to the clipboard
+    When copied, the user is notified by an alert button
+
+extensive console logs have purposefully left in to show program operation if desired
 
 Current unsolved issues:
-pasting the password result back to the html
-does not copy contents to clipboard
+an error on AppendChild
 
--- myJS.js
+++ myJS.js
@@ -2,10 +2,9 @@
 let lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-let specialArray = ["~", "!", "#", "*", "<"];
+let specialArray = ["~", "!", "#", "*", "<",">","&"];
 let sampleArray = [];
-let PWFinal =""
-var copyPW = ""
+let PWFinal = ""
 
 // setting the data from  checkboxes for when button clicked 
 let lowerOption = document.getElementById("lowerCase");
@@ -17,7 +16,7 @@ let specialOption = document.getElementById("specialCharacters");
 document.getElementById("generate-btn").addEventListener("click", function (event) {
 
     //resetting the password in case they are reclicking the button another time
-    let PWFinal ="";
+    let PWFinal = "";
 
     //getting the length from the range slider
     let rangeInput = document.getElementById("passwordLength").value;
@@ -25,82 +24,78 @@ document.getElementById("generate-btn").addEventListener("click", function (even
     //just to show that the password length is set
     console.log("The password length is " + rangeInput);
 
-        //make sure they checked a box (an actual useful use of an alert!)
-        if (lowerOption.checked == false && upperOption.checked == false && numericOption.checked == false &&
-            specialOption.checked == false) {
-            alert("I can't make something out of nothing! You must choose at least one text type...");
-            return;
-        }
+    //make sure they checked a box (an actual useful use of an alert!)
+    if (lowerOption.checked == false && upperOption.checked == false && numericOption.checked == false &&
+        specialOption.checked == false) {
+        alert("I can't make something out of nothing! You must choose at least one text type...");
+        return;
+    }
 
-        //add lower case letters to sampling array if selected
-        if (lowerOption.checked == true) {
-            sampleArray = sampleArray.concat(lowerCaseArray);
+    //add lower case letters to sampling array if selected
+    if (lowerOption.checked == true) {
+        sampleArray = sampleArray.concat(lowerCaseArray);
 
-            //just to show the adding of the lower case array to the sampling array
-            console.log("Lower case is " + lowerOption.checked + " so add it");
-            console.log(sampleArray)
-        }
+        //just to show the adding of the lower case array to the sampling array
+        console.log("Lower case is " + lowerOption.checked + " so add it");
+        console.log(sampleArray)
+    }
 
-        //add upper case letters to sampling array if selected
-        if (upperOption.checked == true) {
-            sampleArray = sampleArray.concat(upperCaseArray);
+    //add upper case letters to sampling array if selected
+    if (upperOption.checked == true) {
+        sampleArray = sampleArray.concat(upperCaseArray);
 
-            //just to show the adding of the upper case array to the sampling array
-            console.log("Upper case is " + upperOption.checked + " so add it");
-            console.log(sampleArray)
-        }
+        //just to show the adding of the upper case array to the sampling array
+        console.log("Upper case is " + upperOption.checked + " so add it");
+        console.log(sampleArray)
+    }
 
-        //add numeric characters to sampling array if selected
-        if (numericOption.checked == true) {
-            sampleArray = sampleArray.concat(numericArray)
+    //add numeric characters to sampling array if selected
+    if (numericOption.checked == true) {
+        sampleArray = sampleArray.concat(numericArray)
 
-            //just to show the adding of the numeric array to the sampling array
-            console.log(`Numeric is ${numericOption.checked} so add it to master array`);
-            console.log(sampleArray)
-        }
+        //just to show the adding of the numeric array to the sampling array
+        console.log(`Numeric is ${numericOption.checked} so add it to master array`);
+        console.log(sampleArray)
+    }
 
-        //add special characters to sampling array if selected
-        if (specialOption.checked == true) {
-            sampleArray = sampleArray.concat(specialArray)
+    //add special characters to sampling array if selected
+    if (specialOption.checked == true) {
+        sampleArray = sampleArray.concat(specialArray)
 
-            //just to show the adding of the special array to the sampling array
-            console.log("Special is " + specialOption.checked) + "so add it";
-            console.log(sampleArray)
-        }
+        //just to show the adding of the special array to the sampling array
+        console.log("Special is " + specialOption.checked) + "so add it";
+        console.log(sampleArray)
+    }
 
-        //generataing the random selections from the sampling array
+    //generataing the random selections from the sampling array
 
-        for (i = 0; i < rangeInput; i++) {
-            currentPWitem = sampleArray[Math.floor(Math.random() * Math.floor(sampleArray.length))];
+    for (i = 0; i < rangeInput; i++) {
+        currentPWitem = sampleArray[Math.floor(Math.random() * Math.floor(sampleArray.length))];
 
-            //just to show that a random letter has been selected
-            console.log(`Randamly chosen item is ${currentPWitem}`);
+        //just to show that a random letter has been selected
+        console.log(`Randamly chosen item is ${currentPWitem}`);
 
-            //adding the selected letter to the end of the password string
-            PWFinal = PWFinal.concat(currentPWitem);
+        //adding the selected letter to the end of the password string
+        PWFinal = PWFinal.concat(currentPWitem);
 
-            //just to show the working password array progress as each letter is added
-            console.log("Add it to end of password  " + PWFinal);
+        //just to show the working password array progress as each letter is added
+        console.log("Add it to end of password  " + PWFinal);
 
-        }
+    }
 
-        //writing it to the page
-        var insertPW = document.getElementById("PWBox");
-        insertPW.textContent = PWFinal;
-        document.body.PWBox.appendChild(insertPW);
+    //writing it to the page
+    var insertPW = document.getElementById("PWBox");
+    insertPW.textContent = PWFinal;
+    document.body.PWBox.appendChild(insertPW);
 
-        //putting the local variable into a global variable to force the result out of the function
-        copyPW ="";
-        copyPW = copyPW.concat(PWFinal);
-
-
-        
 
 });
 
+//button to copy to clipboard
+
 document.getElementById("copy-btn").addEventListener("click", function (event) {
-        var text = document.getElementById("PWBox");
-        text.select();
-        document.execCommand("copy");
-        alert("Your password has been copied to your computer clipboard.");
+    var text = document.getElementById("PWBox");
+    text.select();
+    document.execCommand("copy");
+    alert("Your password has been copied to your computer clipboard.");
 });
